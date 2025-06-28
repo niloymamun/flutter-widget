744fee9d0d0fd64ff80ee8e08230d45a26b41bab
neloy-mamun <neloymamun06@gmail.com>
2025-06-28 22:51:37 +0600

    add row widget

---
+++ lib/row.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          children: [
+            Expanded(child: Container(color: Colors.red, height: 50)),
+            Expanded(child: Container(color: Colors.green, height: 50)),
+            Expanded(child: Container(color: Colors.blue, height: 50)),
+          ],
+        ),
+      ),
+    );
+  }
+}
