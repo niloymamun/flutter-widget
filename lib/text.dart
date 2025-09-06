b076cf9fdc2b233137310cc3c897fe37d767fcde
al-mamun <neloymamun06@gmail.com>
2025-09-06 21:18:33 +0600

    Add New Widget Text

---
+++ lib/text.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: MyTextPage());
+  }
+}
+
+class MyTextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Text Widget")),
+      body: Center(
+        child: Text(
+          'Welcome Coding with mamun',
+          style: TextStyle(
+            fontSize: 40.00,
+            color: Colors.blue,
+            fontStyle: FontStyle.italic,
+            letterSpacing: 10,
+            wordSpacing: 20,
+            fontWeight: FontWeight.w900,
+            backgroundColor: const Color.fromARGB(255, 209, 231, 9),
+            shadows: [
+              Shadow(
+                color: Colors.blueAccent,
+                offset: Offset(2, 1),
+                blurRadius: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
